3b0bc667b46b10d4b827bcc3f6bded1b5beab3cd
Ben6jamin <b.ishimwe1@alustudent.com>
2022-10-18 02:39:13 -0700

    positive and negative vibes

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number}number is positive")
+    print(f"{number} is positive")
 elif number == 0:
-    print(f"{number}number is zero")
+    print(f"{number} is zero")
 else:
-    print(f"{number}negative number")
+    print(f"{number} is negative")
