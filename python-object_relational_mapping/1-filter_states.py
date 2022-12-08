d8d37c4118f148fcc7d7ff0689b2fd2f06a1f547
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2022-12-08 14:39:34 +0200

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N
+""" lists all states where  a name starting with N
 (upper N) from the
-database hbtn_0e_0_usa
+database hbtn_0e_0_usa (select case)
 """
 
 import MySQLdb
