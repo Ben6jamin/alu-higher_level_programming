d753aa59e5188517da079c4ca6126aca56391ecf
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2022-12-09 12:01:12 +0200

    mode state

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ lists all State objects that contain
 the letter a from the database hbtn_0e_6_usa"""
+""" import sys packages for this task"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
