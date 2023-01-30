6aaef9551fb3ddb9ac5f9511de514e05f5f0d797
Ben6jamin <b.ishimwe1@alustudent.com>
2023-01-30 01:45:22 -0800

    kkll

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const myObject = {
-type: 'object',
-value: 12,
-incr: function() {
-this.value++;
-}
+  type: 'object',
+  value: 12,
+  incr: function() {
+    this.value++;
+  }
 };
 console.log(myObject);
 myObject.incr();
