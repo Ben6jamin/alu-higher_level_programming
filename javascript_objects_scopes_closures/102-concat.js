fca7dd8c25a1cd5bb02925a621cf6b2046a24688
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-01-30 14:32:16 +0200

    Update 102-concat.js

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,23 +1,11 @@
 #!/usr/bin/node
-onst fs = require("fs");
+const content = `${dataA}\n${dataB}`;
 
-if (process.argv.length !== 5) {
-console.error("Usage: 102-concat.js <fileA> <fileB> <destination>");
-process.exit(1);
-}
+fs.writeFile(destination, content, errorC => {
+  if (errorC) {
+    console.error(errorC);
+    process.exit(1);
+  }
 
-const [fileA, fileB, destination] = process.argv.slice(2);
-
-fs.readFile(fileA, "utf-8", (errorA, dataA) => {
-if (errorA) {
-console.error(errorA);
-process.exit(1);
-}
-
-fs.readFile(fileB, "utf-8", (errorB, dataB) => {
-if (errorB) {
-console.error(errorB);
-process.exit(1);
-}
+  console.log(`The contents of fileA and fileB have been concatenated and saved in file ${destination}`);
 });
-});  
