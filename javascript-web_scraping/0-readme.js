255ea669fa6e9b63a2a4ddba04bb4dcc77e19774
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-02-05 18:57:52 +0200

    create

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(filePath, 'utf-8', (err, contents) => {
+  err ? console.log(err):console.log(contents.toString());
 });
