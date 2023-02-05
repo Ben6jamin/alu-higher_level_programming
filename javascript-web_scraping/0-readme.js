ed8d27613158bc97858bcf07fac69dc0ce07feee
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-02-05 19:00:27 +0200

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,5 +2,5 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', (err, contents) => {
-  err ? console.log(err):console.log(contents.toString());
+  err ? console.log(err) : console.log(contents.toString());
 });
