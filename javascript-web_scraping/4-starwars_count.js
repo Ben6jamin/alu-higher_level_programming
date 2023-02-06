3bdd315c42efa9d1fe06c4c2ca9b54466a94c392
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-02-06 13:05:48 +0200

    count

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 const API_URL = process.argv[2];
 const characterId = 18;
 
 request(API_URL, function (error, response, body) {
   if (error) {
-    console.error("Error: ", error);
+    console.error('Error: ', error);
     return;
   }
 
