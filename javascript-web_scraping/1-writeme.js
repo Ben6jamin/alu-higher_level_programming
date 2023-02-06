10a009a5d3edcfdf2c0b375b31abde916fe80db0
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-02-06 11:51:27 +0200

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf8', (error) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
+  if (err) console.log(err);
 });
