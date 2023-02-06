6ceebd1ea841cf352072e07c4375e68d0e25445d
Ben6jamin <b.ishimwe1@alustudent.com>
2023-02-06 03:36:45 -0800

    changes

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,21 +1,12 @@
 #!/usr/bin/node
-const request = require('request');
+const req = require('request');
 const fs = require('fs');
-
 const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-    if (writeError) {
-      console.error(writeError);
-      return;
-    }
-    console.log(`The contents of ${url} were saved to ${filePath}.`);
-  });
+const file = process.argv[3];
+req.get(url, async (err, res) => {
+  err
+    ? console.log(err)
+    : await fs.writeFile(file, res.body, (err) => {
+      if (err) console.log(err);
+    });
 });
