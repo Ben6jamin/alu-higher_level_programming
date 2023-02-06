2198148d64942d321e75d27197f1d0475a11588c
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-02-06 12:36:21 +0200

    code updated

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request(url, function (error, response) {
+request(url, function (error, response)) {
   console.log('code: ' + response.statusCode);
 });
